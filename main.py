ac9a524eb1385d969e945250ced00049eef1671f
Aditya Jha <adi4ant@gmail.com>
2023-06-08 08:35:17 +0530

    added insert, search features, and navbar

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 # creating a flask application that will fetch data from the database and display it on the web page
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
 
 def connectToDatabase(user,password,host,database):
-    # connecting to the database
     mydb = mysql.connector.connect(
         user=user,
         password=password,
@@ -23,36 +22,41 @@ def fetchFromDatabase(mydb,query):
 def createApp():
     # creating a flask application
     app = Flask(__name__)
+    mydb = connectToDatabase("root","root","localhost","c361")
+    create_query = "CREATE TABLE IF NOT EXISTS test (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), age INT)"
+    insert_query = "insert into test (name,age) values (%s,%s)"
+    search_query = "select * from test where name = %s"
+    mycursor = mydb.cursor()
 
+
+    # this function will simply fetch data from db and display it on homepage
     @app.route('/')
     def index():
-        # connecting to the database
-        mydb = connectToDatabase("root","root","localhost","c361")
-
-        create_query = "CREATE TABLE IF NOT EXISTS test (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), age INT)"
-        # creating a table in the database
-        mycursor = mydb.cursor()
-        mycursor.execute(create_query)
-
-        insert_query = "insert into test (name,age) values (%s,%s)"
-
-        # inserting data into the database
-
-        data = [
-            ("John",20),
-            ("Amy",22),
-            ("Peter",21),
-            ("Hannah",23)
-        ]
-
-        mycursor.executemany(insert_query,data)
-        mydb.commit()
-
-        # fetching data from the database
         myresult = fetchFromDatabase(mydb,"SELECT * FROM test")
-
-        # rendering the template
         return render_template("index.html", data=myresult)
+    
+
+    # creating a route to insert data into the database using a form
+    @app.route('/insert', methods=['GET','POST'])
+    def insert():
+        if request.method == 'POST':
+            name = request.form['name']
+            age = request.form['age']
+            val = (name,age)
+            mycursor.execute(insert_query,val)
+            mydb.commit()
+            return redirect(url_for('index'))
+        return render_template("insert.html")
+    
+    # creating a route to search data from the database using a form
+    @app.route('/search',methods=['GET','POST'])
+    def search():
+        if request.method == 'POST':
+            name = request.form['name']
+            mycursor.execute(search_query,(name,))
+            result = mycursor.fetchall()
+            return render_template("search.html",data=result)
+        return render_template("search.html")
 
     return app
 
